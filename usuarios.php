e682e6cd1fa2adc00191b0781a52bdff80782c68
Valverde <profissional.matheus.valverde@gmail.com>
2026-03-13 20:03:08 -0300

    Trabalho

---
+++ usuarios.php
@@ -0,0 +1,39 @@
+<main>
+    <div class = "header-conte">
+        <h2>Gestão de Usuário</h2>
+        <p>Visualise e gerencie as permições dos usuários do sistema.</p>
+    </div>
+
+    <div class="table-resposive">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Acesso</th>
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>01</td>
+                    <td>Eric Freitas </td>
+                    <td>eric@unifev.edu.br</td>
+                    <td>Admistrador</td>
+                    <td>Ativo</td>
+                    <td>Editar</td>
+                </tr>
+                <tr>
+                    <td>02</td>
+                    <td>Ana Sousa</td>
+                    <td>ana.sousa@gmail.com</td>
+                    <td>Editor</td>
+                    <td>Ativo</td>
+                    <td>Editar</td>
+                </tr>
+            </tbody>
+        <table>
+    </div>
+</main>
