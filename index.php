3c8d044c2c6d3ced2d794d0eed291c6609a95f54
Valverde <profissional.matheus.valverde@gmail.com>
2026-03-13 22:51:12 -0300

    Inicio

--- index.php
+++ index.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Banco de dados</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    
+    <div class="dashboard-container">
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Relatórios</a></li>
+                <li><a href="#">Configurações</a></li>
+            </ul>
+            <div>
+                <img src="https://ui-avatars.com/api/?name=Eric+freitas&background=008080&color=fff" alt="Avatar">
+                <span>Atirador Valverde °36</span>
+            </div>
+        </nav>
+    </div>
 </body>
 </html>
